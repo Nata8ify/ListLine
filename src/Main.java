58373432b7d83515ce5a41fbe5e09a0a260696fd
nata6ify <nata6ify@gmail.com>
2023-04-13 11:11:27 +0700

    --checkpint v1.1, fix bugs and improve extractor flow

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,21 @@ public class Main {
         listLine();
     }
 
+    private static final String VERSION = "1.1";
+    private static final String RELEASE_DATE = "13/04/2023";
     private static final String IGNORE_KEYWORD_SIGN = "#";
     private static final String CONFIG_SECTION_INPUT_DIRECTORY = "<<InputDirectory>>";
     private static final String CONFIG_SECTION_OUTPUT_DIRECTORY = "<<OutputDirectory>>";
+    private static final String CONFIG_SECTION_IGNORED_INPUT_FILE_CONTAIN_WITH = "<<IgnoredInputFileContainWith>>";
     private static final String CONFIG_SECTION_KEYWORDS = "<<Keywords>>";
     private static final String CONFIG_SECTION_IGNORED_KEYWORDS = "<<IgnoredKeywords>>";
     private static final String CONFIG_SECTION_GROUPED_BY_THREAD_KEYWORDS = "<<GroupedByThreadKeywords>>";
 
     public static void listLine() throws IOException {
+
+        System.out.println("=========================================================");
+        System.out.println("ListLine v" + VERSION + ", ReleaseDate: " + RELEASE_DATE);
+        System.out.println("=========================================================");
 
         String propertiesPathConfig = System.getProperty("properties.ini");
         File properties = new File(propertiesPathConfig == null ? "properties.ini" : propertiesPathConfig);
@@ -35,13 +42,17 @@ public class Main {
         String inputDirectory = null;
         String outputDirectory = null;
         ArrayList<String> keywords = new ArrayList<>();
+        ArrayList<String> ignoredInputFileContainWith = new ArrayList<>();
         ArrayList<String> ignoredKeywords = new ArrayList<>();
         ArrayList<String> groupedByThreadKeywords = new ArrayList<>();
 
         String currentConfigState = "";
         List<String> propertyLines = Files.readAllLines(properties.toPath());
         for (String line : propertyLines) {
-            if (List.of(CONFIG_SECTION_INPUT_DIRECTORY, CONFIG_SECTION_OUTPUT_DIRECTORY, CONFIG_SECTION_KEYWORDS, CONFIG_SECTION_IGNORED_KEYWORDS, CONFIG_SECTION_GROUPED_BY_THREAD_KEYWORDS).contains(line.trim())) {
+            if (line.isBlank()) {
+                continue;
+            }
+            if (List.of(CONFIG_SECTION_INPUT_DIRECTORY, CONFIG_SECTION_OUTPUT_DIRECTORY, CONFIG_SECTION_IGNORED_INPUT_FILE_CONTAIN_WITH, CONFIG_SECTION_KEYWORDS, CONFIG_SECTION_IGNORED_KEYWORDS, CONFIG_SECTION_GROUPED_BY_THREAD_KEYWORDS).contains(line.trim())) {
                 currentConfigState = line.trim();
             } else {
                 switch (currentConfigState) {
@@ -52,6 +63,10 @@ public class Main {
                     case CONFIG_SECTION_OUTPUT_DIRECTORY:
                         outputDirectory = line;
                         System.out.println("Output Directory: " + outputDirectory);
+                        break;
+                    case CONFIG_SECTION_IGNORED_INPUT_FILE_CONTAIN_WITH:
+                        ignoredInputFileContainWith.add(line);
+                        System.out.println("Ignored Input File Contain With Keyword: " + line);
                         break;
                     case CONFIG_SECTION_KEYWORDS:
                         keywords.add(line);
@@ -83,12 +98,18 @@ public class Main {
             try (FileWriter fileWriter = new FileWriter(out)) {
                 System.out.println("Extracting Lines... [" + keyword + "]");
                 AtomicInteger containCount = new AtomicInteger(0);
-                for (File file : Arrays.asList(dir.listFiles()).stream().sorted(Comparator.comparingInt(f -> {
-                    String[] splitted = ((File) f).getName().split("\\.");
-                    return splitted.length == 3 ? Integer.parseInt(splitted[1]) : Integer.MAX_VALUE;
-                })).collect(Collectors.toList())) {
+                for (File file : Arrays.asList(dir.listFiles()).stream().sorted(
+//                        Comparator.comparingInt(f -> { String[] splitted = ((File) f).getName().split("\\."); return splitted.length == 3 ? Integer.parseInt(splitted[1]) : Integer.MAX_VALUE; })
+                        Comparator.comparingLong(File::lastModified)
+                ).collect(Collectors.toList())) {
                     if (!file.isDirectory()) {
-                        List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+
+                        if (ignoredInputFileContainWith.stream().anyMatch(it -> file.getName().contains(it))) {
+                            /*System.out.println("Ignore and skip file [" + file.getName() + "]");*/
+                            continue;
+                        }
+
+                            List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
                         fileWriter.write(file.getName() + "\n");
                         lines.forEach(it -> {
                             for (String ignore : ignoredKeywords) {
